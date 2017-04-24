33d4d2ef7afd90f504b10001f2b67ddfb868222b
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-24 21:43:08 +0200

    Inbox = Messages

---
+++ Source/Inbox/Web/wwwroot/Messages/index.js
@@ -0,0 +1,17 @@
+Bifrost.namespace("Web.Inbox", {
+    index: Bifrost.views.ViewModel.extend(function() {
+        this.messages = ko.observableArray([
+            {
+                from: "Someone",
+                Subject: "Something cool",
+                when: "7 mins ago",
+                starred: false,
+                hasAttachment: false
+            }
+        ]);
+
+        this.tags = ko.observableArray([
+            "Spam"
+        ]);
+    })
+});
