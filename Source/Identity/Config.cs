d119089c631275192238630d6e4f160bc2a2fdf2
Einar Ingebrigtsen <einar@dolittle.com>
2017-04-25 09:50:25 +0200

    Started on an Identity Server

---
+++ Source/Identity/Config.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using IdentityServer4.Models;
+
+namespace Identity
+{
+public class Config
+    {
+        // scopes define the API resources in your system
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource("api1", "My API")
+            };
+        }
+
+        // client want to access resources (aka scopes)
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "client",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+                    AllowedScopes = { "api1" }
+                }
+            };
+        }
+    }}
