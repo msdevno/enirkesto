d605279c9fa3e26ccf7795bacc6e591e1530821b
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-04 08:49:13 +0200

    Basic read side up and running for messages

--- Source/Inbox/Read/Messages/EventProcessors.cs
+++ Source/Inbox/Read/Messages/EventProcessors.cs
@@ -1,15 +1,29 @@
+using System;
 using Bifrost.Events;
+using Bifrost.Read;
 using Events.Messages;
 
 namespace Read.Messages
 {
     public class EventProcessors : IProcessEvents
     {
+        IReadModelRepositoryFor<Message> _messageRepository;
+        public EventProcessors(IReadModelRepositoryFor<Message> messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
+
         public void Process(MessageReceived @event)
         {
-            var i=0;
-            i++;
-            
+            var message = new Message
+            {
+                Inbox = (Guid)@event.EventSourceId,
+                MessageId = @event.MessageId,
+                Subject = @event.Subject,
+                Body = @event.Body
+            };
+
+            _messageRepository.Insert(message);
         }
     }
 }
