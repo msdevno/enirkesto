4a6bfba705792c37bf1be04e01b0d6af08a5771e
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-08 20:37:07 +0200

    cleanup

--- Source/TextAnalytics/main.js
+++ Source/TextAnalytics/main.js
@@ -1,11 +1,3 @@
-// https://github.com/wooorm/franc
-/*
-var franc = require("franc");
-var language = franc("Jeg skulle registrere meg, men det fungerte ikke i det hele tatt")
-console.log(`Language : ${language}`);
-*/
-
-
 //require("./tagModelImporter").import("./models/kid.txt");
 
 // Services that we expose
