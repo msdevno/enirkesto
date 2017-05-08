c8c12d826191c336333b83817dce1d2fb41ae121
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-08 08:39:40 +0200

    Setup scripts

---
+++ setup.cmd
@@ -0,0 +1,5 @@
+mklink /D Source\Kernel\Concepts Source\Inbox\Concepts
+mklink /D Source\Kernel\Events Source\Inbox\Events
+
+mklink /D Source\Kernel\Concepts Source\TextAnalytics.dotnet\Concepts
+mklink /D Source\Kernel\Events Source\TextAnalytics.dotnet\Events
---
+++ setup.sh
@@ -0,0 +1,6 @@
+cd ./Source/Inbox
+ln -s ../Kernel/Concepts Concepts
+ln -s ../Kernel/Events Events
+cd ../TextAnalytics.dotnet
+ln -s ../Kernel/Concepts Concepts
+ln -s ../Kernel/Events Events
