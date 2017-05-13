251ccca947663425db0f98535d0993c668a039b0
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-13 09:38:16 +0200

    Adding optional language in the request

--- Source/TextAnalytics/api/classify.js
+++ Source/TextAnalytics/api/classify.js
@@ -7,11 +7,11 @@ app.post("/models/:type/classify", (req, res) => {
     Expected JSON Payload:
     {
         "key": "a unique key for the model",
+        "language": "optional - if not present, we'll try to detect it. iso639 - 2 letter representation e.g. nb for Norwegian Bokmål"
         "text": "Text to classify - get tags for"
     }
     */
-
-    let lang = languages.detect(req.body.text);
+    let lang = req.body.language || languages.detect(req.body.text);
     models.getFor(req.body.key, req.params.type, lang).then(model => {
         let result = model.getClassificationsFor(req.body.text);
         res.status(200).json(result);
