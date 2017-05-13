06a1e4ae1e5909bfa377a639a420a19a6d773f3a
Einar Ingebrigtsen <einar@dolittle.com>
2017-05-13 10:58:19 +0200

    Minor

--- Source/Inbox/Web/Program.cs
+++ Source/Inbox/Web/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.PlatformAbstractions;
 
 namespace Web
 {
